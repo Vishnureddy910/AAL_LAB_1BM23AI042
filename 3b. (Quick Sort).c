a2d1e7af8b0b23aca9bb0f2ed3d1a70aa2541323
Vishnureddy910 <vishnu133reddy@gmail.com>
2025-05-15 22:49:14 +0530

    Create 3b. (Quick Sort).c

---
+++ 3b. (Quick Sort).c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot = arr[high];
+        int i = low - 1, temp;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        int pi = i + 1;
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[100], n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    quickSort(arr, 0, n - 1);
+    printf("Sorted array:\n");
+    printArray(arr, n);
+
+    return 0;
+}
